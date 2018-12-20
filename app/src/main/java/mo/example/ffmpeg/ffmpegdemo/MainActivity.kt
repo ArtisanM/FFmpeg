720913db7294e42ebd235a41f4e917de2c0bf3c8
mo.duan <duanmo@qingting.fm>
2018-12-20 19:21:04 +0800

    支持ffmpeg命令行

--- app/src/main/java/mo/example/ffmpeg/ffmpegdemo/MainActivity.kt
+++ app/src/main/java/mo/example/ffmpeg/ffmpegdemo/MainActivity.kt
@@ -1,20 +1,16 @@
 package mo.example.ffmpeg.ffmpegdemo
 
 import android.Manifest
+import android.media.MediaMetadataRetriever
 import android.os.Bundle
 import android.os.Environment
 import android.support.v4.app.ActivityCompat
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
-import kotlinx.android.synthetic.main.activity_main.*
 import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
-    external fun avformatInfo(): String
-    external fun avcodecInfo(): String
-    external fun avfilterInfo(): String
-    external fun configurationInfo(): String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,34 +18,41 @@ class MainActivity : AppCompatActivity() {
         ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),100)
 
         // Example of a call to a native method
-        sample_text.text = avfilterInfo()
+//        sample_text.text = avfilterInfo()
 
-//        val duration = endTime - startTime;
-//// 构建一条视频裁剪命令
-//        val cmd = CmdList();
-//        cmd.append("ffmpeg");
-//        cmd.append("-y");
-//        cmd.append("-ss").append(startTime/ 1000).append("-t").append(duration / 1000).append("-accurate_seek");
-//        cmd.append("-i").append(srcFile);
-//        cmd.append("-codec").append("copy").append(destFile);
-//
-//        FFmpegUtil.execCmd(cmd, duration)
     }
 
-    companion object {
-
-        // Used to load the 'native-lib' library on application startup.
-        init {
-            System.loadLibrary("FFmpegUtil")
-        }
-    }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == 100) {
             if (grantResults.isNotEmpty()) {
-                val file = File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), "music.mp3")
-                Log.e("MainActivity", "isExist:${file.exists()}  path:${file.absolutePath}")
+                val file = File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), "forhim.m4a")
+                Log.e("FFmpeg_VideoEditor", "isExist:${file.exists()}  path:${file.absolutePath}")
+
+                val out = File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), "forhim_cut.m4a")
+                out.delete()
+
+                val mediaMetadataRetriever = MediaMetadataRetriever()
+                mediaMetadataRetriever.setDataSource(file.absolutePath)
+                val duration = mediaMetadataRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION).toLong()
+                mediaMetadataRetriever.release()
+                Log.e("FFmpeg_VideoEditor", "duration $duration")
+
+                FFmpegCmd.exec("ffmpeg -i ${file.absolutePath} -ss 00:00:00 -t 40 -vsync 2 -c copy ${out.absolutePath}".split(" ").toTypedArray() , duration, object : FFmpegCmd.OnCmdExecListener{
+                    override fun onSuccess() {
+                        Log.e("FFmpeg_VideoEditor", "ffmpeg cmd exec success ${out.exists()}")
+                    }
+
+                    override fun onFailure() {
+                        Log.e("FFmpeg_VideoEditor", "ffmpeg cmd exec failed")
+                    }
+
+                    override fun onProgress(progress: Float) {
+                        Log.e("FFmpeg_VideoEditor", "ffmpeg cmd exec onProgress $progress")
+                    }
+
+                })
             }
         }
     }
